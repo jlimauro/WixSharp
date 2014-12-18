5794ba1f57e93cc46b46a3c32b87a56fa3885c65
oleg_s <oshilO@gmail.com>
2014-12-19 10:12:09 +1100

    Added missing files

---
+++ src/WixSharp.Test/GenericTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using Xunit;
+
+namespace WixSharp.Test
+{
+    //none of the existing UnitTests are migrated yet from the old codebase
+    public class GenericTest
+    {
+        [Fact]
+        public void Test()
+        {
+            Compiler.GuidGenerator = GuidGenerators.Default;
+            Compiler.GuidGenerator = GuidGenerators.Sequential;
+            Compiler.GuidGenerator = (seed) => Guid.Parse("9e2974a1-9539-4c5c-bef7-80fc35b9d7b0");
+            Compiler.GuidGenerator = (seed) => Guid.NewGuid();
+            
+            var ttt = WixGuid.NewGuid();
+            var rrr = WixGuid.NewGuid();
+        }
+    }
+}
--- src/WixSharp.Test/Properties/AssemblyInfo.cs
+++ src/WixSharp.Test/Properties/AssemblyInfo.cs
@@ -1,26 +1,26 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WixSharp")]
+[assembly: AssemblyTitle("WixSharp.Test")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Oleg Shilo")]
-[assembly: AssemblyProduct("WixSharp")]
-[assembly: AssemblyCopyright("Copyright Oleg Shilo 2008-2014")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WixSharp.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
-// to COM componenets.  If you need to access a type in this assembly from 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8afbd3fc-0a39-4db5-b592-b2981982b33d")]
+[assembly: Guid("170bd2f7-ef8f-4db9-bd71-391009504f7a")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.3.0")]
-[assembly: AssemblyFileVersion("1.0.3.0")]
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
