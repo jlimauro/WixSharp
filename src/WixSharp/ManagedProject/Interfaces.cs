5e30f89ccf60cc7647280778ec80bb611829ced6
oleg_s <oshilO@gmail.com>
2015-05-20 14:17:09 +1000

    Added missing files

---
+++ src/WixSharp/ManagedProject/Interfaces.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WixSharp
+{
+    public interface IManagedDialog
+    {
+    }
+    public interface IManagedProgressDialog:IManagedDialog
+    {
+    }
+
+    public interface IManagedUI
+    {
+        ManagedDialogs InstallDialogs { get; }
+        ManagedDialogs RepairDialogs { get; }
+    }
+
+    public class ManagedDialogs : List<Type>
+    {
+        //public ManagedDialogs Add(Type dialog)
+        //{
+        //    if (dialog.FindInterfaces()
+        //    base.Add(dialog);
+        //    return this;
+        //}
+
+        public ManagedDialogs Add<T>() where T : IManagedDialog
+        {
+            base.Add(typeof(T));
+            return this;
+        }
+
+        public ManagedDialogs Clear()
+        {
+            base.Clear();
+            return this;
+        }
+    }
+}
