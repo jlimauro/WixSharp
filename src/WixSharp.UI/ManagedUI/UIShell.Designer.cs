c5e60a9c7329f45a780c819683618b1a787445d3
oleg_s <oshilO@gmail.com>
2015-05-20 16:12:58 +1000

    Added missing files

---
+++ src/WixSharp.UI/ManagedUI/UIShell.Designer.cs
@@ -0,0 +1,173 @@
+﻿using System.Windows.Forms;
+namespace WixSharp
+{
+    partial class UIShell
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        private TextBox sequenceView;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sequenceView = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.cancel = new System.Windows.Forms.Button();
+            this.next = new System.Windows.Forms.Button();
+            this.install = new System.Windows.Forms.Button();
+            this.exit = new System.Windows.Forms.Button();
+            this.status = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.currentStep = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // sequenceView
+            // 
+            this.sequenceView.Location = new System.Drawing.Point(0, 0);
+            this.sequenceView.Name = "sequenceView";
+            this.sequenceView.Size = new System.Drawing.Size(100, 20);
+            this.sequenceView.TabIndex = 0;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(13, 213);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(226, 69);
+            this.listBox1.TabIndex = 0;
+            // 
+            // cancel
+            // 
+            this.cancel.Location = new System.Drawing.Point(320, 308);
+            this.cancel.Name = "cancel";
+            this.cancel.Size = new System.Drawing.Size(75, 23);
+            this.cancel.TabIndex = 1;
+            this.cancel.Text = "Cancel";
+            this.cancel.UseVisualStyleBackColor = true;
+            this.cancel.Click += new System.EventHandler(this.cancel_Click);
+            // 
+            // next
+            // 
+            this.next.Location = new System.Drawing.Point(144, 303);
+            this.next.Name = "next";
+            this.next.Size = new System.Drawing.Size(75, 23);
+            this.next.TabIndex = 1;
+            this.next.Text = "[Next]";
+            this.next.UseVisualStyleBackColor = true;
+            this.next.Click += new System.EventHandler(this.next_Click);
+            // 
+            // install
+            // 
+            this.install.Location = new System.Drawing.Point(239, 308);
+            this.install.Name = "install";
+            this.install.Size = new System.Drawing.Size(75, 23);
+            this.install.TabIndex = 1;
+            this.install.Text = "Install";
+            this.install.UseVisualStyleBackColor = true;
+            this.install.Click += new System.EventHandler(this.Install_Click);
+            // 
+            // exit
+            // 
+            this.exit.Cursor = System.Windows.Forms.Cursors.Default;
+            this.exit.Enabled = false;
+            this.exit.Location = new System.Drawing.Point(491, 308);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(75, 23);
+            this.exit.TabIndex = 1;
+            this.exit.Text = "Exit";
+            this.exit.UseVisualStyleBackColor = true;
+            this.exit.Click += new System.EventHandler(this.exit_Click);
+            // 
+            // status
+            // 
+            this.status.AutoSize = true;
+            this.status.Location = new System.Drawing.Point(23, 308);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(35, 13);
+            this.status.TabIndex = 2;
+            this.status.Text = "label1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 42);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(553, 165);
+            this.textBox1.TabIndex = 3;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(13, 24);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(553, 12);
+            this.progressBar.TabIndex = 4;
+            // 
+            // currentStep
+            // 
+            this.currentStep.AutoSize = true;
+            this.currentStep.Location = new System.Drawing.Point(10, 8);
+            this.currentStep.Name = "currentStep";
+            this.currentStep.Size = new System.Drawing.Size(35, 13);
+            this.currentStep.TabIndex = 2;
+            this.currentStep.Text = "label1";
+            // 
+            // UIShell
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(578, 362);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.currentStep);
+            this.Controls.Add(this.status);
+            this.Controls.Add(this.next);
+            this.Controls.Add(this.exit);
+            this.Controls.Add(this.install);
+            this.Controls.Add(this.cancel);
+            this.Controls.Add(this.listBox1);
+            this.MaximizeBox = false;
+            this.Name = "UIShell";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Managed UI Setup";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ListBox listBox1;
+        private Button cancel;
+        private Button next;
+        private Button install;
+        private Button exit;
+        private Label status;
+        private TextBox textBox1;
+        private ProgressBar progressBar;
+        private Label currentStep;
+
+    }
+}
